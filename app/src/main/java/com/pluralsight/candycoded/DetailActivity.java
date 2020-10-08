f7dad242cfbc825ff7a1dbac3b519731eb1f7cfa
Sydney Ozegbe <43884387+sydneyoz@users.noreply.github.com>
2020-10-08 22:17:57 +0100

    Share a Candy with an Intent

--- app/src/main/java/com/pluralsight/candycoded/DetailActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/DetailActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -59,6 +60,21 @@ public class DetailActivity extends AppCompatActivity {
                     R.id.image_view_candy);
             Picasso.with(this).load(mCandyImageUrl).into(imageView);
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        createShareIntent();
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void createShareIntent() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+
+        String shareString = SHARE_DESCRIPTION + mCandyImageUrl + HASHTAG_CANDYCODED;
+        shareIntent.putExtra(Intent.EXTRA_TEXT, shareString);
+        startActivity(shareIntent);
     }
 
     @Override
